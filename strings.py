f1ed6f9fac2295a5668d8e41af19a4849b83e603
Mohan Sai Adavikayala <118970152+19G21A0503@users.noreply.github.com>
2023-09-23 20:47:00 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,43 @@
+
+
+#String
+#Implicit
+a = "Mohansai"
+print(a)
+print(type(a))
+
+#Explicit
+a = str("Mohansai")
+print(a)
+print(type(a))
+
+#Variable Annotation
+a: str = "Mohansai"
+print(a)
+print(type(a))
+
+#Boolean
+a = str(True)
+print(a)
+print(type(a))
+
+#Octal
+a = str(0o1234)
+print(a)
+print(type(a))
+
+#HexaDecimal
+a = str(0xface)
+print(a)
+print(type(a))
+
+#Binary
+a = str(0b1010)
+print(a)
+print(type(a))
+
+#Decimal
+#integer
+a = str(1234)
+print(a)
+print(type(a))
